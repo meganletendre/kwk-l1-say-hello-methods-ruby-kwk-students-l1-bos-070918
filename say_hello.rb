04c746ad160591a9a597a5ccc4ea1fc5c1a00507
meganletendre <megan.letendre@comcast.net>
2018-07-10 15:00:15 +0000

    automatically backed up by learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,7 +1,7 @@
 # Build your say_hello method here
 
 #puts "Please enter your name: "
-#your_name = gets.chomp
+your_name = gets.chomp
 
 def say_hello (your_name, my_name)
   puts "Hello, #{your_name}!I'm #{my_name}"
